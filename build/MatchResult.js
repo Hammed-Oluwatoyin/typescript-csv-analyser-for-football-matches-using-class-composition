0ecb61ec77fcd78d1c7e26f43fcf57920cc761b6
Dauda Hammed <dauda.oluwatoyin.hammed@gmail.com>
2020-07-16 01:06:56 +0100

    final commit

--- build/MatchResult.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MatchResults;
-(function (MatchResults) {
-    MatchResults["homeWin"] = "H";
-    MatchResults["awayWin"] = "A";
-    MatchResults["Draw"] = "D";
-})(MatchResults = exports.MatchResults || (exports.MatchResults = {}));
--- build/MatchResults.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MatchResults;
-(function (MatchResults) {
-    MatchResults["HomeWins"] = "H";
-    MatchResults["AwayWins"] = "A";
-    MatchResults["Draw"] = "D";
-})(MatchResults = exports.MatchResults || (exports.MatchResults = {}));
